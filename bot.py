a07fea0710b2a0c5cad82ea4f2335d72dbcb8220
attilakk-gif <attilakk@gmail.com>
2026-03-30 20:46:24 +0200

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,25 @@
+from telegram.ext import Updater, MessageHandler, Filters
+import os
+
+TOKEN = os.getenv("TOKEN")
+
+PAROLA_CHIAVE = "ciao"
+RISPOSTA = "Ciao! Come va?"
+
+def ascolta(update, context):
+    if update.message and update.message.text:
+        testo = update.message.text.lower()
+        if PAROLA_CHIAVE in testo:
+            update.message.reply_text(RISPOSTA)
+
+def main():
+    updater = Updater(TOKEN, use_context=True)
+    dp = updater.dispatcher
+
+    dp.add_handler(MessageHandler(Filters.text & ~Filters.command, ascolta))
+
+    updater.start_polling()
+    updater.idle()
+
+if __name__ == "__main__":
+    main()
